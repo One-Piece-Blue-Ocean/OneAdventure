eba033ad32c8126161623a42a6497a43ad929ea9
Thang Nguyen <Thang14nguyen@gmail.com>
2023-06-09 15:46:43 -0700

    add themes for the app

---
+++ client/screens/Themes.jsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+
+const muted = {
+  gold: 'f9bf23',
+  red: 'd52b1e',
+  blue: '1c8fd2',
+  orange: 'ff7043',
+  peach: 'bc8f8f',
+  black: '33333',
+  white: 'white',
+};
+
+const myTheme = {
+  dark: false,
+  colors: {
+    primary: 'rgb(255, 45, 85)',
+    background: muted.white,
+    card: 'rgb(255, 255, 255)',
+    text: muted.black,
+    border: 'rgb(199, 199, 204)',
+    notification: 'rgb(255, 69, 58)',
+  },
+};
+
+export default myTheme;
