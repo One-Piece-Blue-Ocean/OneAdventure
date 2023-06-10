c0566070ec84fafe227d465ac10c6937f1d1e7f5
Noah Beito <noah.beito@gmail.com>
2023-06-10 15:42:44 -0700

    built out functionality and generic styling for SignUp and Login Screens

--- client/index.jsx
+++ client/index.jsx
@@ -1,22 +1,41 @@
-import React from 'react';
 import 'react-native-gesture-handler';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { decode, encode } from 'base-64';
 
 import LoginScreen from './screens/Login';
 import SignUpScreen from './screens/SignUp';
 import NavScreen from './screens/Nav';
 
+if (!global.btoa) { global.btoa = encode; }
+if (!global.atob) { global.atob = decode; }
+
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="Nav" component={NavScreen} />
+        {
+          user ? (
+            <Stack.Screen name="Nav">
+              {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+              {(props) => <NavScreen {...props} extraData={user} />}
+            </Stack.Screen>
+          ) : (
+            <>
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen name="SignUp" component={SignUpScreen} />
+            </>
+          )
+        }
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+
--- client/screens/Login.jsx
+++ client/screens/Login.jsx
@@ -1,22 +1,123 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  StyleSheet, Text, View, Button,
+  StyleSheet, Image, Text, TextInput, TouchableOpacity, View, Button,
 } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import PropTypes from 'prop-types';
+import icon from '../../assets/icon.png';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+  },
+  title: {
+
+  },
+  logo: {
+    flex: 1,
+    height: 120,
+    width: 90,
+    alignSelf: 'center',
+    margin: 30,
+  },
+  input: {
+    height: 48,
+    borderRadius: 5,
+    overflow: 'hidden',
     backgroundColor: 'white',
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    paddingLeft: 16,
+  },
+  button: {
+    backgroundColor: '#788eec',
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 20,
+    height: 48,
+    borderRadius: 5,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  buttonTitle: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  footerView: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  footerText: {
+    fontSize: 16,
+    color: '#2e2e2d',
+  },
+  footerLink: {
+    color: '#788eec',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
 
 function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onFooterLinkPress = () => {
+    navigation.navigate('SignUp');
+  };
+
+  const onLoginPress = () => {
+
+  };
+
   return (
     <View style={styles.container}>
-      <Text> Login </Text>
+      <KeyboardAwareScrollView
+        style={{ flex: 1, width: '100%' }}
+        keyboardShouldPersistTaps="always"
+      >
+        <Image
+          sytle={styles.logo}
+          source={icon}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="E-mail"
+          placeholderTextColor="#aaaaaa"
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          placeholderTextColor="#aaaaaa"
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => onLoginPress()}
+        >
+          <Text style={styles.buttonTitle}>Log in</Text>
+        </TouchableOpacity>
+        <View style={styles.footerView}>
+          <Text style={styles.footerText}>
+            Don&apos;t have an account?
+            <Text onPress={onFooterLinkPress} style={styles.footerLink}>
+              &nbsp;Sign up
+            </Text>
+          </Text>
+        </View>
+      </KeyboardAwareScrollView>
       <Button
         title="Nav"
         onPress={() => {
