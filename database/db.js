40660b22c5537c50423a7dbb566d050aa8d0f99e
Jacqueline-Kelly <Jacqueline.kelly.2016@gmail.com>
2023-06-10 11:31:58 -0700

    disabled lint error for extraneous dependency

--- database/db.js
+++ database/db.js
@@ -1,14 +1,15 @@
-import { initializeApp } from 'firebase/app';
+/* eslint-disable import/no-extraneous-dependencies */
+const { initializeApp } = require('firebase/app');
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDV7F5RquNgMNhcQ_URcfPGZhgGhyZ-Roo",
-  authDomain: "oneadventure-c0c45.firebaseapp.com",
-  projectId: "oneadventure-c0c45",
-  storageBucket: "oneadventure-c0c45.appspot.com",
-  messagingSenderId: "821027623464",
-  appId: "1:821027623464:web:46d3956de086789f391b1d",
-  measurementId: "G-8N1D89NHTG"
+  apiKey: 'AIzaSyDV7F5RquNgMNhcQ_URcfPGZhgGhyZ-Roo',
+  authDomain: 'oneadventure-c0c45.firebaseapp.com',
+  projectId: 'oneadventure-c0c45',
+  storageBucket: 'oneadventure-c0c45.appspot.com',
+  messagingSenderId: '821027623464',
+  appId: '1:821027623464:web:46d3956de086789f391b1d',
+  measurementId: 'G-8N1D89NHTG',
 };
 
 module.exports.app = initializeApp(firebaseConfig);
