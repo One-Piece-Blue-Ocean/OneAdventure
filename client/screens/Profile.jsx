60acb7f96f664af0e61bdc6349e3e483b6a9a059
jtoneill <tyleroneill21@gmail.com>
2023-06-10 23:11:19 -0700

    Added friends list.

--- client/screens/Profile.jsx
+++ client/screens/Profile.jsx
@@ -4,9 +4,12 @@ import {
   Text,
   View,
   Image,
-  TextInput,
+  SafeAreaView,
+  FlatList,
+  StatusBar,
 } from 'react-native';
 import PropTypes from 'prop-types';
+import myTheme from './Themes';
 
 const styles = StyleSheet.create({
   button: {
@@ -16,12 +19,36 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
   details: {
     flexDirection: 'row',
+    backgroundColor: myTheme.colors.border,
+  },
+  friendsHeader: {
+    width: '100%',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  friendsHeaderText: {
+    fontSize: 24,
+  },
+  friendsListContainer: {
+    width: '100%',
+    flex: 1,
+    backgroundColor: myTheme.colors.border,
+    alignItems: 'center',
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  friendCardContainer: {
+    width: 350,
+    height: 100,
+    margin: 10,
+    backgroundColor: 'white',
+    borderRadius: 4,
+    shadowColor: 'black',
   },
   input: {
     height: 40,
@@ -36,13 +63,17 @@ const styles = StyleSheet.create({
   },
   profileImage: {
     flex: 1,
-    top: 0,
-    left: 0,
+    marginLeft: 10,
   },
   userNameContainer: {
     flex: 3,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  userNameText: {
+    fontSize: 25,
+    borderRadius: 10,
+    overflow: 'hidden',
   },
   profileOptions: {
     flex: 1,
@@ -50,10 +81,56 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
-    margin: 10,
+  shadowProps: {
+    shadowOffset: { width: -2, height: 2 },
+    shadowColor: 'black',
+    shadowOpacity: 0.8,
+    shadowRadius: 3,
   },
+  text: {
+
+  },
+  textWrap: {
+    backgroundColor: 'white',
+    padding: 10,
+    margin: 3,
+    borderRadius: 3,
+    overflow: 'hidden',
+  },
+
 });
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    name: 'First Item',
+    profilePic: 'https://www.workforcesolutionsalamo.org/wp-content/uploads/2021/04/board-member-missing-image.png',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    name: 'Second Item',
+    profilePic: 'https://www.workforcesolutionsalamo.org/wp-content/uploads/2021/04/board-member-missing-image.png',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    name: 'Third Item',
+    profilePic: 'https://www.workforcesolutionsalamo.org/wp-content/uploads/2021/04/board-member-missing-image.png',
+  },
+];
+
+function FriendCard({ friend }) {
+  console.log('this is friend', friend.name);
+  return (
+    <View style={styles.friendCardContainer}>
+      <View>
+        <Text style={styles.text}>{friend.name}</Text>
+      </View>
+      <View>
+        <Text style={styles.text}>{friend.id}</Text>
+      </View>
+    </View>
+  );
+}
 
 function ProfileScreen() {
   const [userName, setUserName] = useState('@Buckey');
@@ -63,7 +140,7 @@ function ProfileScreen() {
   const [modalVisible, setModalVisible] = useState(false);
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.profileImageContainer}>
         <Image
           styles={styles.profileImage}
@@ -74,26 +151,38 @@ function ProfileScreen() {
           }}
         />
         <View style={styles.userNameContainer}>
-          <Text>{userName}</Text>
+          <Text style={styles.userNameText}>{userName}</Text>
         </View>
       </View>
       <View style={styles.details}>
         <View style={styles.profileOptions}>
-          <Text style={styles.text}>{location}</Text>
-          <Text style={styles.text}>{searchRadius}</Text>
+          <View style={styles.textWrap}>
+            <Text style={styles.text}>{location}</Text>
+          </View>
+          <View style={styles.textWrap}>
+            <Text style={styles.text}>{searchRadius}</Text>
+          </View>
         </View>
         <View style={styles.profileOptions}>
-          <Text style={styles.text}>email</Text>
-          <Text style={styles.text}>{typePreference}</Text>
+          <View style={styles.textWrap}>
+            <Text style={styles.text}>email</Text>
+          </View>
+          <View style={styles.textWrap}>
+            <Text style={styles.text}>{typePreference}</Text>
+          </View>
         </View>
       </View>
-      <View>
-        <Text>Friends List</Text>
+      <View style={styles.friendsHeader}>
+        <Text style={styles.friendsHeaderText}>Friends List</Text>
       </View>
       <View style={styles.friendsListContainer}>
-
+        <FlatList
+          data={DATA}
+          renderItem={({ item }) => <FriendCard friend={item} />}
+          keyExtractor={(friend) => friend.id}
+        />
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
