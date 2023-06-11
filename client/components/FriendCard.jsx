7bdf3692e1aea72e644f467e6fd6dd4fd3b52117
jtoneill <tyleroneill21@gmail.com>
2023-06-11 15:58:25 -0700

    Created a friendCard for the friends list. Added buttons to message and remove friends. Added a modal to verify removing friends. Adjusted the details layout.

---
+++ client/components/FriendCard.jsx
@@ -0,0 +1,197 @@
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  StatusBar,
+  TouchableOpacity,
+  Modal,
+} from 'react-native';
+import PropTypes from 'prop-types';
+import { Entypo, FontAwesome } from '@expo/vector-icons';
+import myTheme from '../screens/Themes';
+
+const styles = StyleSheet.create({
+  centerModal: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  friendsHeaderContainer: {
+    width: '100%',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    MarginTop: 10,
+  },
+  friendsHeaderText: {
+    fontSize: 24,
+  },
+  friendsListContainer: {
+    width: '100%',
+    flex: 1,
+    backgroundColor: myTheme.colors.border,
+    alignItems: 'center',
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  friendCardContainer: {
+    width: 350,
+    height: 70,
+    margin: 10,
+    backgroundColor: 'white',
+    borderRadius: 4,
+    flexDirection: 'row',
+    flex: 1,
+  },
+  friendCardDetails: {
+    flexDirection: 'row',
+    flex: 4,
+  },
+  friendNameContainer: {
+    padding: 10,
+    margin: 3,
+    borderRadius: 3,
+    overflow: 'hidden',
+    flexDirection: 'row',
+    alignItems: 'center',
+    flex: 1.25,
+  },
+  friendNameText: {
+    fontSize: 18,
+  },
+  friendBtnContainer: {
+    margin: 2,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flex: 1,
+  },
+  friendProfileImageContainer: {
+    height: '100%',
+    flex: 1,
+  },
+  modalBtnContainer: {
+    flexDirection: 'row',
+  },
+  modalButton: {
+    borderRadius: 5,
+    padding: 10,
+    elevation: 2,
+    backgroundColor: 'skyblue',
+    margin: 20,
+    marginBottom: 0,
+  },
+  modalContainer: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+  },
+  profileImageWrap: {
+    height: 50,
+    margin: 10,
+    borderRadius: 50,
+    overflow: 'hidden',
+    marginLeft: 10,
+  },
+});
+
+function FriendCard({ friend }) {
+  const [modalVisible, setModalVisible] = useState(false);
+  const { profilePic, userName, id } = friend;
+
+  const onMessage = (friendId) => {
+    console.log('start or go to message with friend that was clicked, id:', friendId);
+  };
+
+  const onRemove = (friendId) => {
+    console.log('remove friend from friends list in db, id:', friendId);
+  };
+
+  return (
+    <View style={styles.friendCardContainer}>
+      <View style={styles.friendProfileImageContainer}>
+        <View style={styles.profileImageWrap}>
+          <Image
+            source={{
+              uri: profilePic,
+              height: 50,
+              width: 50,
+            }}
+          />
+        </View>
+      </View>
+      <View style={styles.friendCardDetails}>
+        <View style={styles.friendNameContainer}>
+          <Text style={styles.friendNameText}>{userName}</Text>
+        </View>
+        <View style={styles.friendBtnContainer}>
+          <TouchableOpacity>
+            <Entypo
+              name="message"
+              size={24}
+              color="black"
+              onPress={() => onMessage(id)}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <FontAwesome
+              name="remove"
+              size={24}
+              color="red"
+              onPress={() => setModalVisible(true)}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <Modal
+        animationType="fade"
+        transparent
+        visible={modalVisible}
+      >
+        <View style={styles.centerModal}>
+          <View style={styles.modalContainer}>
+            <Text style={styles.modalText}>Remove from friends list?</Text>
+            <View style={styles.modalBtnContainer}>
+              <TouchableOpacity
+                style={styles.modalButton}
+                onPress={() => {
+                  onRemove(id);
+                  setModalVisible(false);
+                }}
+              >
+                <Text>Remove</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.modalButton}
+                onPress={() => setModalVisible(false)}
+              >
+                <Text>Cancel</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+}
+
+FriendCard.propTypes = {
+  friend: PropTypes.shape({
+    userName: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    profilePic: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default FriendCard;
--- client/screens/Profile.jsx
+++ client/screens/Profile.jsx
@@ -9,7 +9,9 @@ import {
   StatusBar,
 } from 'react-native';
 import PropTypes from 'prop-types';
+import { AntDesign } from '@expo/vector-icons';
 import myTheme from './Themes';
+import FriendCard from '../components/FriendCard';
 
 const styles = StyleSheet.create({
   button: {
@@ -23,10 +25,13 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
   },
   details: {
-    flexDirection: 'row',
     backgroundColor: myTheme.colors.border,
+    width: '100%',
+    height: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  friendsHeader: {
+  friendsHeaderContainer: {
     width: '100%',
     height: 40,
     justifyContent: 'center',
@@ -42,44 +47,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: StatusBar.currentHeight || 0,
   },
-  friendCardContainer: {
-    width: 350,
-    height: 100,
-    margin: 10,
-    backgroundColor: 'white',
-    borderRadius: 4,
-    shadowColor: 'black',
-  },
   input: {
     height: 40,
     margin: 12,
     padding: 10,
     borderWidth: 1,
   },
-  profileImageContainer: {
-    width: '100%',
-    height: 100,
+  profileHeader: {
     flexDirection: 'row',
   },
-  profileImage: {
-    flex: 1,
+  profileImageContainer: {
+    borderRadius: 50,
+    overflow: 'hidden',
     marginLeft: 10,
   },
   userNameContainer: {
     flex: 3,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'white',
   },
   userNameText: {
     fontSize: 25,
     borderRadius: 10,
     overflow: 'hidden',
-  },
-  profileOptions: {
-    flex: 1,
-    marginTop: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   shadowProps: {
     shadowOffset: { width: -2, height: 2 },
@@ -88,52 +79,62 @@ const styles = StyleSheet.create({
     shadowRadius: 3,
   },
   text: {
-
+    fontSize: 12,
   },
   textWrap: {
     backgroundColor: 'white',
-    padding: 10,
-    margin: 3,
+    width: '90%',
+    padding: 2,
+    margin: 5,
     borderRadius: 3,
     overflow: 'hidden',
+    flexDirection: 'row',
   },
-
+  option: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
 });
 
+// temp friend data
 const DATA = [
   {
     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    name: 'First Item',
+    userName: 'Dave',
     profilePic: 'https://www.workforcesolutionsalamo.org/wp-content/uploads/2021/04/board-member-missing-image.png',
   },
   {
     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    name: 'Second Item',
+    userName: 'Steve',
     profilePic: 'https://www.workforcesolutionsalamo.org/wp-content/uploads/2021/04/board-member-missing-image.png',
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    name: 'Third Item',
+    userName: 'Jim',
+    profilePic: 'https://www.workforcesolutionsalamo.org/wp-content/uploads/2021/04/board-member-missing-image.png',
+  },
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28bz',
+    userName: 'Sara',
+    profilePic: 'https://www.workforcesolutionsalamo.org/wp-content/uploads/2021/04/board-member-missing-image.png',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f6z',
+    userName: 'Billy',
+    profilePic: 'https://www.workforcesolutionsalamo.org/wp-content/uploads/2021/04/board-member-missing-image.png',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d7z',
+    userName: 'Chris',
     profilePic: 'https://www.workforcesolutionsalamo.org/wp-content/uploads/2021/04/board-member-missing-image.png',
   },
 ];
 
-function FriendCard({ friend }) {
-  console.log('this is friend', friend.name);
-  return (
-    <View style={styles.friendCardContainer}>
-      <View>
-        <Text style={styles.text}>{friend.name}</Text>
-      </View>
-      <View>
-        <Text style={styles.text}>{friend.id}</Text>
-      </View>
-    </View>
-  );
-}
-
 function ProfileScreen() {
-  const [userName, setUserName] = useState('@Buckey');
+  const [data, setData] = useState(DATA);
+  const [userName, setUserName] = useState('Buckey');
   const [location, setLocation] = useState('lat, lon?');
   const [searchRadius, setSearchRadius] = useState(50);
   const [typePreference, setTypePreference] = useState('hiking');
@@ -141,43 +142,71 @@ function ProfileScreen() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.profileImageContainer}>
-        <Image
-          styles={styles.profileImage}
-          source={{
-            uri: 'https://www.workforcesolutionsalamo.org/wp-content/uploads/2021/04/board-member-missing-image.png',
-            height: 100,
-            width: 100,
-          }}
-        />
+      <View style={styles.profileHeader}>
+        <View style={styles.profileImageContainer}>
+          <Image
+            source={{
+              uri: 'https://www.workforcesolutionsalamo.org/wp-content/uploads/2021/04/board-member-missing-image.png',
+              height: 100,
+              width: 100,
+            }}
+          />
+        </View>
         <View style={styles.userNameContainer}>
           <Text style={styles.userNameText}>{userName}</Text>
         </View>
       </View>
       <View style={styles.details}>
-        <View style={styles.profileOptions}>
-          <View style={styles.textWrap}>
-            <Text style={styles.text}>{location}</Text>
+        <View style={styles.textWrap}>
+          <View style={styles.option}>
+            <Text style={styles.text}>Email:</Text>
           </View>
-          <View style={styles.textWrap}>
-            <Text style={styles.text}>{searchRadius}</Text>
+          <View style={styles.option}>
+            <Text style={styles.text}>buckey@mail.com</Text>
+            <AntDesign name="edit" size={20} color="black" />
           </View>
         </View>
-        <View style={styles.profileOptions}>
-          <View style={styles.textWrap}>
-            <Text style={styles.text}>email</Text>
+        <View style={styles.textWrap}>
+          <View style={styles.option}>
+            <Text style={styles.text}>Default Type:</Text>
           </View>
-          <View style={styles.textWrap}>
+          <View style={styles.option}>
             <Text style={styles.text}>{typePreference}</Text>
+            <AntDesign name="edit" size={20} color="black" />
+          </View>
+        </View>
+        <View style={styles.textWrap}>
+          <View style={styles.option}>
+            <Text style={styles.text}>Defualt Location:</Text>
+          </View>
+          <View style={styles.option}>
+            <Text style={styles.text}>{location}</Text>
+            <AntDesign name="edit" size={20} color="black" />
+          </View>
+        </View>
+        <View style={styles.textWrap}>
+          <View style={styles.option}>
+            <Text style={styles.text}>Default Search Radius:</Text>
+          </View>
+          <View style={styles.option}>
+            <Text style={styles.text}>{searchRadius}</Text>
+            <AntDesign
+              name="edit"
+              size={20}
+              color="black"
+              onPress={() => {
+
+              }}
+            />
           </View>
         </View>
       </View>
-      <View style={styles.friendsHeader}>
+      <View style={styles.friendsHeaderContainer}>
         <Text style={styles.friendsHeaderText}>Friends List</Text>
       </View>
       <View style={styles.friendsListContainer}>
         <FlatList
-          data={DATA}
+          data={data}
           renderItem={({ item }) => <FriendCard friend={item} />}
           keyExtractor={(friend) => friend.id}
         />
