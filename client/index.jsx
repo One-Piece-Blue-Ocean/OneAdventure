e35fbc792b62aa8805936209301458920b4e0b66
Keith Foreman <keithforeman218@gmail.com>
2023-06-12 09:54:45 -0700

    add initial placeholder for detail page

--- client/index.jsx
+++ client/index.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-<<<<<<< HEAD
 import { NativeBaseProvider } from 'native-base';
-=======
 import { decode, encode } from 'base-64';
->>>>>>> 6d9692648a69a2fbc3b77e4ade58e1b27186071f
 
 import LoginScreen from './screens/Login';
 import SignUpScreen from './screens/SignUp';
 import NavScreen from './screens/Nav';
+import AdventureDetail from './screens/AdventureDetail';
 
 if (!global.btoa) { global.btoa = encode; }
 if (!global.atob) { global.atob = decode; }
@@ -22,6 +20,7 @@ export default function App() {
     <NavigationContainer>
       <NativeBaseProvider>
         <Stack.Navigator>
+          <Stack.Screen name="Detail" component={AdventureDetail} />
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="SignUp" component={SignUpScreen} />
           <Stack.Screen name="Nav" component={NavScreen} options={{ headerShown: false }} />
