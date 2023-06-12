5acc0242ed70ffeb9ae8114becb3212856cd8f1f
Noah Beito <noah.beito@gmail.com>
2023-06-12 12:47:20 -0700

    commented out context in Nav screen

--- client/screens/Nav.jsx
+++ client/screens/Nav.jsx
@@ -1,10 +1,8 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import PropTypes from 'prop-types';
 import { FontAwesome, Entypo, FontAwesome5 } from '@expo/vector-icons';
 
-// import AdventureListScreen from './AdventureList';
-// import AdventureMapScreen from './AdventureMap';
 import AdventureToggle from './AdventureToggle';
 import AdventureTrackingScreen from './AdventureTracking';
 import ProfileScreen from './Profile';
@@ -17,54 +15,53 @@ const messageIcon = () => <Entypo name="message" size={24} color="black" />;
 const trackerIcon = () => <FontAwesome5 name="walking" size={24} color="black" />;
 const profileIcon = () => <FontAwesome name="user" size={24} color="black" />;
 
-function Nav({ route }) {
-  const UserContext = useContext(route.params);
-  // route.params contains email, fullName, zipcode of current user
-  console.log('USER OBJ: ', route.params); // logging for visibility - remove later
+// const UserContext = createContext(null);
 
+function Nav({ route }) {
+  // route.params contains uid, email, fullName, city of current user
+  const user = route.params;
+  console.log('USER OBJ: ', user); // logging for visibility - remove later
   return (
-    <UserContext.Provider>
-      <Tab.Navigator>
-        {/* <Tab.Screen name="AdventureList" component={AdventureListScreen} /> */}
-        {/* <Tab.Screen name="AdventureMap" component={AdventureMapScreen} /> */}
-        <Tab.Screen
-          name="Adventures"
-          component={AdventureToggle}
-          options={{
-            tabBarLabel: '',
-            tabBarIcon: adventureIcon,
-            headerShown: false,
-          }}
-        />
-        <Tab.Screen
-          name="Messaging"
-          component={MessagingScreen}
-          options={{
-            tabBarLabel: '',
-            tabBarIcon: messageIcon,
-            headerShown: false,
-          }}
-        />
-        <Tab.Screen
-          name="AdventureTracking"
-          component={AdventureTrackingScreen}
-          options={{
-            tabBarLabel: '',
-            tabBarIcon: trackerIcon,
-            headerShown: false,
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{
-            tabBarLabel: '',
-            tabBarIcon: profileIcon,
-            headerShown: false,
-          }}
-        />
-      </Tab.Navigator>
-    </UserContext.Provider>
+  // <UserContext.Provider value={user}>
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Adventures"
+        component={AdventureToggle}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: adventureIcon,
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Messaging"
+        component={MessagingScreen}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: messageIcon,
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="AdventureTracking"
+        component={AdventureTrackingScreen}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: trackerIcon,
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: profileIcon,
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  // </UserContext.Provider>
   );
 }
 
