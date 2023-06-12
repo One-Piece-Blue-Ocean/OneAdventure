a6d0265240c310702e45ba44449edc445f5909d3
Keith Foreman <keithforeman218@gmail.com>
2023-06-12 15:40:56 -0700

    add initialParams

--- client/index.jsx
+++ client/index.jsx
@@ -11,14 +11,15 @@ import NavScreen from './screens/Nav';
 import AdventureDetail from './screens/AdventureDetail';
 
 const testEvent = {
-  name: 'A name',
-  category: 'A category',
-  location: 'A location',
-  description: 'A description',
-  date: 'A Date',
-  star: false,
-  friend: [],
-  imageUrl: 'A url',
+  name: 'Testing the Test',
+  category: 'Hiking',
+  location: 'To the unknown',
+  // eslint-disable-next-line max-len
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  date: '11/3/23',
+  star: true,
+  friends: [],
+  imageUrl: 'https://images.unsplash.com/photo-1682686580950-960d1d513532?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80',
 };
 
 if (!global.btoa) { global.btoa = encode; }
