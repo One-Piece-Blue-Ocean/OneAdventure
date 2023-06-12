2a8eebdd728cb923fa5c0652f9c15747d7729bb9
Noah Beito <noah.beito@gmail.com>
2023-06-12 11:41:37 -0700

    add uid field to data to be stored in database. Add user info to useContext

--- client/screens/Nav.jsx
+++ client/screens/Nav.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import PropTypes from 'prop-types';
 import { FontAwesome, Entypo, FontAwesome5 } from '@expo/vector-icons';
@@ -17,52 +17,54 @@ const messageIcon = () => <Entypo name="message" size={24} color="black" />;
 const trackerIcon = () => <FontAwesome5 name="walking" size={24} color="black" />;
 const profileIcon = () => <FontAwesome name="user" size={24} color="black" />;
 
-
 function Nav({ route }) {
-    // route.params contains email, fullName, zipcode of current user
-    console.log('USER OBJ: ', route.params); // logging for visibility - remove later
+  const UserContext = useContext(route.params);
+  // route.params contains email, fullName, zipcode of current user
+  console.log('USER OBJ: ', route.params); // logging for visibility - remove later
 
   return (
-    <Tab.Navigator>
-      {/* <Tab.Screen name="AdventureList" component={AdventureListScreen} /> */}
-      {/* <Tab.Screen name="AdventureMap" component={AdventureMapScreen} /> */}
-      <Tab.Screen
-        name="Adventures"
-        component={AdventureToggle}
-        options={{
-          tabBarLabel: '',
-          tabBarIcon: adventureIcon,
-          headerShown: false,
-        }}
-      />
-      <Tab.Screen
-        name="Messaging"
-        component={MessagingScreen}
-        options={{
-          tabBarLabel: '',
-          tabBarIcon: messageIcon,
-          headerShown: false,
-        }}
-      />
-      <Tab.Screen
-        name="AdventureTracking"
-        component={AdventureTrackingScreen}
-        options={{
-          tabBarLabel: '',
-          tabBarIcon: trackerIcon,
-          headerShown: false,
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{
-          tabBarLabel: '',
-          tabBarIcon: profileIcon,
-          headerShown: false,
-        }}
-      />
-    </Tab.Navigator>
+    <UserContext.Provider>
+      <Tab.Navigator>
+        {/* <Tab.Screen name="AdventureList" component={AdventureListScreen} /> */}
+        {/* <Tab.Screen name="AdventureMap" component={AdventureMapScreen} /> */}
+        <Tab.Screen
+          name="Adventures"
+          component={AdventureToggle}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: adventureIcon,
+            headerShown: false,
+          }}
+        />
+        <Tab.Screen
+          name="Messaging"
+          component={MessagingScreen}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: messageIcon,
+            headerShown: false,
+          }}
+        />
+        <Tab.Screen
+          name="AdventureTracking"
+          component={AdventureTrackingScreen}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: trackerIcon,
+            headerShown: false,
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: profileIcon,
+            headerShown: false,
+          }}
+        />
+      </Tab.Navigator>
+    </UserContext.Provider>
   );
 }
 
@@ -72,9 +74,10 @@ Nav.propTypes = {
     name: PropTypes.string.isRequired,
     params: PropTypes.shape({
       user: PropTypes.shape({
+        uid: PropTypes.string.isRequired,
         email: PropTypes.string.isRequired,
         fullName: PropTypes.string.isRequired,
-        zipcode: PropTypes.string.isRequired,
+        city: PropTypes.string.isRequired,
       }).isRequired,
     }).isRequired,
     path: PropTypes.string,
