d15e327e0eca64d1963ca4973e09a48455a453e2
Jacqueline-Kelly <Jacqueline.kelly.2016@gmail.com>
2023-06-12 17:13:35 -0700

    card component & favoriting on cards

--- client/screens/AdventureTracking.jsx
+++ client/screens/AdventureTracking.jsx
@@ -1,9 +1,15 @@
+/* eslint-disable */
 import React, { useContext, useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView, } from 'react-native';
 import {
-  collection, doc, getDoc, getDocs,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  updateDoc
 } from 'firebase/firestore';
 import { db } from '../../database/db';
+import Card from '../components/card.jsx';
 
 const styles = StyleSheet.create({
   container: {
@@ -22,17 +28,38 @@ function AdventureTrackingScreen() {
   const adventureRef = collection(db, 'adventures');
 
   const getAdventures = () => {
+    let currentUserEvent;
     getDocs(userAdventuresRef)
       .then((userEventsDocs) => {
         const promiseArr = userEventsDocs.docs.map((eventDoc) => {
+          currentUserEvent = eventDoc.data();
           const adventureDoc = doc(adventureRef, eventDoc.data().adventureId);
-          return getDoc(adventureDoc).then((adventureDocData) => adventureDocData.data());
+          return getDoc(adventureDoc).then((adventureDocData) => {
+            return {[eventDoc.id]: {
+              userAdventureInfo: currentUserEvent,
+              adventureInfo: adventureDocData.data()}
+            }
+          });
         });
         Promise.all(promiseArr).then((resolvedAdventures) => {
           setAdventuresList(resolvedAdventures);
         });
       });
   };
+
+  const handleInterested = (userEventId, interested) => {
+    const docReference = doc(db, 'pirates', userId, 'events', userEventId)
+    updateDoc(docReference, {'interested': !interested[0]})
+    .then(() => {
+      getDoc(docReference)
+      .then((updatedDoc) => {
+        let objIndToUpdate = adventuresList.findIndex((obj) => Object.keys(obj)[0] === userEventId);
+        let newState = [...adventuresList];
+        newState[objIndToUpdate][userEventId]['userAdventureInfo'] = updatedDoc.data();
+        setAdventuresList(newState);
+      })
+    })
+  }
 
   useEffect(() => {
     if (!adventuresList.length) {
@@ -41,18 +68,22 @@ function AdventureTrackingScreen() {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text> AdventureTracking </Text>
-      {adventuresList.length
-        ? adventuresList.map((adventure) => (
-          <View key={adventure.eventName} style={styles.container}>
-            <Text>
-              {adventure.eventName}
-            </Text>
-          </View>
-        ))
-        : null}
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <Text> AdventureTracking </Text>
+        {adventuresList.length
+          ? adventuresList.map((adventure) => (
+            <Card
+              key={Math.random()}
+              event={Object.values(adventure)[0].adventureInfo}
+              userEvent={Object.values(adventure)[0].userAdventureInfo}
+              userEventId={Object.keys(adventure)[0]}
+              loaded
+              handleInterested={handleInterested} />
+          ))
+          : null}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
