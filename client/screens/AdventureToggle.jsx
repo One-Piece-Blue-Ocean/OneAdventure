00c5227fb705d926792002120ce75cf659083242
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2023-06-13 12:03:59 -0700

    commiting changes

--- client/screens/AdventureToggle.jsx
+++ client/screens/AdventureToggle.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import axios from 'axios';
 import AdventureListScreen from './AdventureList';
 import AdventureMapScreen from './AdventureMap';
+// import UserContext from '../context';
 
 const AdventureStack = createStackNavigator();
 
+// function AdventureMapScreenComponent() {
+//   return <AdventureMapScreen events={events} />;
+// }
+const keys = ['d7730fb56dd377a7f809600c8bb2ea1622d13b7ce05c86a10620a95af633c739', '1a751689270a58ec94e8d4cbf5b736e038363ef8547d5b4c566c70a333f8e34e', 'a269b59e99709ed38a523f42def0f8931b4b94c01496d831a6ab795b688971dd', '354e63609f6a81073cc39e6dd08fc7741c620fe41136f235e113287f1c418618', 'c56eb2ab8e762526f911dd86ac06aba293e66e3131bf3546f6555d9d73a56158', '7b89682fcdfde216d3e2205284b1a957e56ce3de484caf5d7f7c00b57057b27a'];
+
 function AdventureToggleScreen() {
+  // const userId = useContext('userContext');
+  // const { city } = useContext(UserContext);
+  const [events, setEvents] = useState([]);
+
+  const city = 'San Francisco';
+
+  useEffect(() => {
+    if (events.length === 0) {
+      axios.get('https://serpapi.com/search?engine=google_events', {
+        params: {
+          api_key: keys[Math.round(Math.random() * 5 + 1)],
+          q: `outdoor activities in ${city}`,
+        },
+      })
+        .then((response) => {
+          console.log('AAAAAA');
+          console.log(response.data.events_results);
+          setEvents(response.data.events_results);
+        })
+        .catch((err) => {
+          console.log('JAKJDKFJ');
+          console.log(err);
+        });
+    }
+  }, []);
+
+  if (events.length) {
+    console.log(events);
+  }
+
   return (
     <AdventureStack.Navigator>
       <AdventureStack.Screen
@@ -15,8 +52,8 @@ function AdventureToggleScreen() {
       />
       <AdventureStack.Screen
         name="AdventureMap"
-        component={AdventureMapScreen}
         options={{ headerShown: false }}
+        children={() => <AdventureMapScreen events={events} />}
       />
     </AdventureStack.Navigator>
   );
