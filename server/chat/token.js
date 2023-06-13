ebe9c406264f55d7368e526ac30f89c9affe01af
Thang Nguyen <Thang14nguyen@gmail.com>
2023-06-12 21:28:02 -0700

    updating to address lint and testing

--- server/chat/token.js
+++ server/chat/token.js
@@ -1,17 +1,6 @@
 // useChatClient.js
-
-import { useEffect, useState } from 'react';
 import { StreamChat } from 'stream-chat';
-import { chatApiKey } from './stream.config';
 
-const chatClient = StreamChat.getInstance(chatApiKey);
-
-function useChatClient() {
-  const [clientIsReady, setClientIsReady] = useState(false);
-
-  return {
-    clientIsReady,
-  };
-};
+const chatClient = StreamChat.getInstance(process.env.CHAT_KEY);
 
 export default chatClient;
