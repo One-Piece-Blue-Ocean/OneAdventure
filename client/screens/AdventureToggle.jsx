da7e6dfbc905dfe11dc9f39a0abe06232284a670
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2023-06-15 11:04:39 -0700

    added a laoder icon for when the new search area is loading, did some styling on the search this area button

--- client/screens/AdventureToggle.jsx
+++ client/screens/AdventureToggle.jsx
@@ -69,6 +69,7 @@ function AdventureToggleScreen() {
             <AdventureMapScreen
               // eslint-disable-next-line react/jsx-props-no-spreading
               {...props}
+              search={search}
               setSearch={setSearch}
             />
           )}
