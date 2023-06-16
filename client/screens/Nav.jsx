06b028682c25cd31b7819ecc55ca66764dd6b0c8
Keith Foreman <keithforeman218@gmail.com>
2023-06-16 09:40:02 -0700

    toggle color of nav

--- client/screens/Nav.jsx
+++ client/screens/Nav.jsx
@@ -71,8 +71,11 @@ function Nav({ route }) {
           tabBarStyle: {
             backgroundColor: route.name === 'Adventures'
               || route.name === 'AdventureTracking' ? '#00A5E0' : '#FF2F00',
+            paddingTop: 12,
           },
-          paddingTop: 12,
+          tabBarLabelStyle: {
+            display: 'none',
+          },
         })}
       >
         <Tab.Screen
